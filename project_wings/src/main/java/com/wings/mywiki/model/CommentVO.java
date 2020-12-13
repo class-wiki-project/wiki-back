e47b9739db113b4d32ddae4ed0cf965c64a8225d
dbswl4951 <dbswl31024@naver.com>
2020-12-13 22:53:24 +0900

    2020_12_13 윤지 --- 댓글에 학생 이름 추가

--- project_wings/src/main/java/com/wings/mywiki/model/CommentVO.java
+++ project_wings/src/main/java/com/wings/mywiki/model/CommentVO.java
@@ -1,5 +1,11 @@
 package com.wings.mywiki.model;
 
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.DateSerializer;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -10,5 +16,7 @@ import lombok.ToString;
 public class CommentVO {
 	private int commentId;
 	private int boardId,userId;	//외래키
-	private String noticeDate,commentText;
+	private String commentText;
+	@JsonFormat(pattern="yyyy-MM-dd")
+	private Date noticeDate;
 }
