ceb097d39a78999dc651d94ad439657aadbcb0cd
dbswl4951 <dbswl31024@naver.com>
2020-12-20 23:29:38 +0900

    2020_12_20 윤지 --- adminController form

--- project_wings/src/main/java/com/wings/mywiki/controller/AdminController.java
+++ project_wings/src/main/java/com/wings/mywiki/controller/AdminController.java
@@ -1,5 +1,10 @@
 package com.wings.mywiki.controller;
 
-public class AdminController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+	
 }
