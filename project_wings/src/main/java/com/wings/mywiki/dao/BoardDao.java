2dd8b3379e7909800f7c5810f7de0e2186988c9d
SujinJeong <64065318+SujinJeong@users.noreply.github.com>
2020-12-22 03:20:02 +0900

    12/22 utf-8 설정 변경

--- project_wings/src/main/java/com/wings/mywiki/dao/BoardDao.java
+++ project_wings/src/main/java/com/wings/mywiki/dao/BoardDao.java
@@ -12,20 +12,12 @@ import com.wings.mywiki.model.Criteria;
 import com.wings.mywiki.model.SubjectVO;
 
 public interface BoardDao {
-	//게시글 작성
 	public int createPost(HashMap<String, Object> map) throws DataAccessException;
-	//게시글 상세보기
 	public BoardVO viewPostDetail(int postId) throws DataAccessException;
-	//게시글 수정
 	public int updatePost(HashMap<String, Object> map) throws DataAccessException;
-	//게시글 삭제
 	public void deletePost(int postId) throws DataAccessException;
-	//게시글 전체목록 조회
 	public List<BoardVO> listAll(Criteria cri) throws DataAccessException;
-	//게시글 조회수
 	public void increaseViewCnt(int postId) throws DataAccessException;
-	//게시글 전체 데이터수
 	public int getTotalCount(Criteria cri) throws DataAccessException;
-	//해당 Board 객체 불러오기
 	public BoardVO getBoard(int boardId) throws DataAccessException;
 }
