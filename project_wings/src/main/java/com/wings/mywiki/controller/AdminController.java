9106207dd167109efaa235643791f5aef4ce4b9d
dbswl4951 <dbswl31024@naver.com>
2020-12-21 16:58:25 +0900

    2020_12_21

--- project_wings/src/main/java/com/wings/mywiki/controller/AdminController.java
+++ project_wings/src/main/java/com/wings/mywiki/controller/AdminController.java
@@ -7,10 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.wings.mywiki.model.CommentVO;
 import com.wings.mywiki.model.ReportVO;
 import com.wings.mywiki.service.AdminService;
 
